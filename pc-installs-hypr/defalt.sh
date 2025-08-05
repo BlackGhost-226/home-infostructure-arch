4d9e608103418f4d256b6363541efe75a5a338f8
BlackGhost-226 <bg.BlackGhost226@gmail.com>
2025-08-05 12:01:06 +0200

    Update defalt.sh

--- pc-installs-hypr/defalt.sh
+++ pc-installs-hypr/defalt.sh
@@ -30,8 +30,8 @@ if [[ $INST == "Y" || $INST == "y" ]]; then
     swappy grim slurp pamixer brightnessctl gvfs \
     bluez bluez-utils lxappearance xfce4-settings \
     dracula-gtk-theme dracula-icons-git xdg-desktop-portal-hyprland\
-    wlroots xdg-utils wayland libegl vulkan-icd-loader mesa polkit-gnome\
-    greetd greetd-tuigreet vulkan-nouveau
+    wlroots xdg-utils wayland libegl vulkan-icd-loader mesa\
+    greetd greetd-tuigreet vulkan-nouveau dolphin
 
     # Start the bluetooth service
     echo -e "Starting the Bluetooth Service...\n"
