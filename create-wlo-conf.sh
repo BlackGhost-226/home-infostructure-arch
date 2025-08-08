ce6ab3577bb72ef598f7858c14f6c01a4a2ba1d3
BlackGhost-226 <bg.BlackGhost226@gmail.com>
2025-08-08 15:21:01 +0200

    new files

---
+++ create-wlo-conf.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.config/wlogout
+cp /etc/wlogout/layout ~/.config/wlogout
+cp /etc/wlogout/style.css ~/.config/wlogout
---
+++ install-yay.sh
@@ -0,0 +1,8 @@
+sudo yes | git clone https://aur.archlinux.org/yay-git.git /opt
+
+USER="$(id -run)"
+sudo chown -R $USER:$USER /opt/yay-git
+
+yes | makepkg -D /opt/yay-git -si
+
+sudo yay -Suy --noconfirm
