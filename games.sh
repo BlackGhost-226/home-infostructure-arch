c88dc098c9429e755b3eca812789360d58963dd3
BlackGhost-226 <bg.BlackGhost226@gmail.com>
2026-06-02 22:47:17 +0200

    Update games.sh

--- games.sh
+++ games.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "[multilib]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 yay -S --noconfirm steam heroic-games-launcher protonplus atlauncher
